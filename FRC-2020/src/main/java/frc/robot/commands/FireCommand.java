cecadca853b674c6c80b16721746c9fba56e1a93
TrashPaenda <benvigil2001@gmail.com>
2020-02-11 18:55:42 -0700

    Update FireCommand.java

--- FRC-2020/src/main/java/frc/robot/commands/FireCommand.java
+++ FRC-2020/src/main/java/frc/robot/commands/FireCommand.java
@@ -7,10 +7,9 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.command.WaitCommand;
+//import edu.wpi.first.wpilibj.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
-import frc.robot.subsystems.ManipulatorSubsystem;
 
 public class FireCommand extends CommandBase {
   /**
